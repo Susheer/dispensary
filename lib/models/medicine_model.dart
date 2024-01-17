ef3cd29bc7cdd88fb59262c01c3129831f39f768
sudheer <sudheergupta0508@gmail.com>
2024-01-17 14:43:50 +0530

    chatGPT: Models and SQL Injection for Prescription, Prescription Line, Medicines added.

---
+++ lib/models/medicine_model.dart
@@ -0,0 +1,36 @@
+// medicine_model.dart
+class Medicine {
+  final int sysMedicineId;
+  final String name;
+  final String description;
+  final DateTime createdDate;
+  final DateTime updatedDate;
+
+  Medicine({
+    required this.sysMedicineId,
+    required this.name,
+    required this.description,
+    required this.createdDate,
+    required this.updatedDate,
+  });
+
+  Map<String, dynamic> toMap() {
+    return {
+      'sys_medicine_id': sysMedicineId,
+      'name': name,
+      'description': description,
+      'created_date': createdDate.toIso8601String(),
+      'updated_date': updatedDate.toIso8601String(),
+    };
+  }
+
+  factory Medicine.fromMap(Map<String, dynamic> map) {
+    return Medicine(
+      sysMedicineId: map['sys_medicine_id'],
+      name: map['name'],
+      description: map['description'],
+      createdDate: DateTime.parse(map['created_date']),
+      updatedDate: DateTime.parse(map['updated_date']),
+    );
+  }
+}
---
+++ lib/models/prescription_model.dart
@@ -0,0 +1,59 @@
+import 'prescription_line_model.dart';
+
+class Prescription {
+  final int sysPrescriptionId;
+  final List<PrescriptionLine> prescriptionLines;
+  final int patientId;
+  final String details;
+  final String diagnosis;
+  final String problem;
+  final DateTime createdDate;
+  final DateTime updatedDate;
+  final double totalAmount;
+  final double paidAmount;
+
+  Prescription({
+    required this.sysPrescriptionId,
+    required this.prescriptionLines,
+    required this.patientId,
+    required this.details,
+    required this.diagnosis,
+    required this.problem,
+    required this.createdDate,
+    required this.updatedDate,
+    required this.totalAmount,
+    required this.paidAmount,
+  });
+
+  Map<String, dynamic> toMap() {
+    return {
+      'sys_prescription_id': sysPrescriptionId,
+      'prescription_lines':
+          prescriptionLines.map((line) => line.toMap()).toList(),
+      'patient_id': patientId,
+      'details': details,
+      'diagnosis': diagnosis,
+      'problem': problem,
+      'created_date': createdDate.toIso8601String(),
+      'updated_date': updatedDate.toIso8601String(),
+      'total_amount': totalAmount,
+      'paid_amount': paidAmount,
+    };
+  }
+
+  factory Prescription.fromMap(Map<String, dynamic> map) {
+    return Prescription(
+      sysPrescriptionId: map['sys_prescription_id'],
+      prescriptionLines: List<PrescriptionLine>.from(map['prescription_lines']
+          .map((line) => PrescriptionLine.fromMap(line))),
+      patientId: map['patient_id'],
+      details: map['details'],
+      diagnosis: map['diagnosis'],
+      problem: map['problem'],
+      createdDate: DateTime.parse(map['created_date']),
+      updatedDate: DateTime.parse(map['updated_date']),
+      totalAmount: map['total_amount'],
+      paidAmount: map['paid_amount'],
+    );
+  }
+}
