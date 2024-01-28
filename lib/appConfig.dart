bde1bbcb4cd966c31e3febe0d3cd432a0eb5ab13
sudheer <sudheergupta0508@gmail.com>
2024-01-28 19:47:11 +0530

    Me: meta for generated pdf

--- lib/appConfig.dart
+++ lib/appConfig.dart
@@ -2,4 +2,8 @@ class AppConfig {
   static const String appName = 'Dispensary App';
   static const int PageSize = 10;
   static const int PrescriptionSize = 2;
+  static const String prescriptionPDFAuther = "Sai clinic maharashtra";
+  static const String creator = "Sai clinic app";
+  static const String clinicAddress = "Clinic Address";
+  static const String clinicMobile = "38292993899";
 }
